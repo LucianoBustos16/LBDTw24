26e3fecd85bb8f575d14de1c75e1cd67dd52ba8f
Luciano Bustos <bustosluciano89@gmail.com>
2024-01-28 15:21:39 -0300

    Configuraciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lbdtw-astro.vercel.app/",
+  integrations: [
+    tailwind(),
+    preact()]
+});
